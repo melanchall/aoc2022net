2499d646cf5787f1ca3edeb9134c455a5c9ec5cc
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-12-02 12:27:28 +0300

    Day 2 solved

--- Aoc2022Net.Tests/Days/Day2Tests.cs
+++ Aoc2022Net.Tests/Days/Day2Tests.cs
@@ -3,8 +3,14 @@ using NUnit.Framework;
 
 namespace Aoc2022Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"A Y
+                    B X
+                    C Z", 15)]
+    [DayDataPart1(10816)]
+    [DayDataPart2(@"A Y
+                    B X
+                    C Z", 12)]
+    [DayDataPart2(11657)]
     [TestFixture]
     public sealed class Day2Tests : DayTests<Day2Tests>
     {
--- Aoc2022Net/Days/Day2.cs
+++ Aoc2022Net/Days/Day2.cs
@@ -2,8 +2,31 @@
 {
     internal sealed class Day2 : Day
     {
-        public override object SolvePart1() => null;
+        private static readonly int[][] OutcomesTable =
+        {
+            new[] { 1, 2, 0 },
+            new[] { 0, 1, 2 },
+            new[] { 2, 0, 1 }
+        };
 
-        public override object SolvePart2() => null;
+        public override object SolvePart1() => CalculateTotalScore((abcI, xyzI) =>
+            (xyzI + 1) + OutcomesTable[abcI][xyzI] * 3);
+
+        public override object SolvePart2() => CalculateTotalScore((abcI, xyzI) =>
+            (Array.IndexOf(OutcomesTable[abcI], xyzI) + 1) + xyzI * 3);
+
+        private int CalculateTotalScore(Func<int, int, int> calculate)
+        {
+            const string abc = "ABC";
+            const string xyz = "XYZ";
+
+            return InputData.GetInputLines().Sum(line =>
+            {
+                var parts = line.Split(' ');
+                var abcI = abc.IndexOf(parts[0][0]);
+                var xyzI = xyz.IndexOf(parts[1][0]);
+                return calculate(abcI, xyzI);
+            });
+        }
     }
 }
