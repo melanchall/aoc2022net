08841e7481b6b9cdf678cab04dd6eb7b149d253f
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-12-03 11:03:47 +0300

    Day 3 solved

--- Aoc2022Net.Tests/Days/Day3Tests.cs
+++ Aoc2022Net.Tests/Days/Day3Tests.cs
@@ -3,8 +3,20 @@ using NUnit.Framework;
 
 namespace Aoc2022Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"vJrwpWtwJgWrhcsFMMfFFhFp
+                    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                    PmmdzqPrVvPwwTWBwg
+                    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                    ttgJtRGJQctTZtZT
+                    CrZsJsPPZsGzwwsLwLmpwMDw", 157)]
+    [DayDataPart1(7850)]
+    [DayDataPart2(@"vJrwpWtwJgWrhcsFMMfFFhFp
+                    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+                    PmmdzqPrVvPwwTWBwg
+                    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+                    ttgJtRGJQctTZtZT
+                    CrZsJsPPZsGzwwsLwLmpwMDw", 70)]
+    [DayDataPart2(2581)]
     [TestFixture]
     public sealed class Day3Tests : DayTests<Day3Tests>
     {
--- Aoc2022Net/Days/Day3.cs
+++ Aoc2022Net/Days/Day3.cs
@@ -2,8 +2,21 @@
 {
     internal sealed class Day3 : Day
     {
-        public override object SolvePart1() => null;
+        public override object SolvePart1() => InputData
+            .GetInputLines()
+            .Sum(rucksack => CalculateTotalPriority(rucksack[..(rucksack.Length / 2)], rucksack[(rucksack.Length / 2)..]));
 
-        public override object SolvePart2() => null;
+        public override object SolvePart2() => InputData
+            .GetInputLines()
+            .Chunk(3)
+            .Sum(CalculateTotalPriority);
+
+        private static int CalculateTotalPriority(params string[] chunks)
+        {
+            var commonItem = chunks.Aggregate((result, part) => new string(result.Intersect(part).ToArray())).First();
+            return char.IsLower(commonItem)
+                ? (commonItem - 'a') + 1
+                : (commonItem - 'A') + 27;
+        }
     }
 }
