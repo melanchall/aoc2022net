6d9f977d70decd2db8f1d70bf66e16555657bd32
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-12-04 11:07:16 +0300

    Day 4 solved

--- Aoc2022Net.Tests/Days/Day4Tests.cs
+++ Aoc2022Net.Tests/Days/Day4Tests.cs
@@ -3,8 +3,20 @@ using NUnit.Framework;
 
 namespace Aoc2022Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"2-4,6-8
+                    2-3,4-5
+                    5-7,7-9
+                    2-8,3-7
+                    6-6,4-6
+                    2-6,4-8", 2)]
+    [DayDataPart1(536)]
+    [DayDataPart2(@"2-4,6-8
+                    2-3,4-5
+                    5-7,7-9
+                    2-8,3-7
+                    6-6,4-6
+                    2-6,4-8", 4)]
+    [DayDataPart2(845)]
     [TestFixture]
     public sealed class Day4Tests : DayTests<Day4Tests>
     {
--- Aoc2022Net/Days/Day4.cs
+++ Aoc2022Net/Days/Day4.cs
@@ -1,9 +1,23 @@
-﻿namespace Aoc2022Net.Days
+﻿using Aoc2022Net.Utilities;
+using System.Text.RegularExpressions;
+
+namespace Aoc2022Net.Days
 {
     internal sealed class Day4 : Day
     {
-        public override object SolvePart1() => null;
+        private record AssignmentsPair(Range First, Range Second);
 
-        public override object SolvePart2() => null;
+        public override object SolvePart1() => GetAssignmentsPairs()
+            .Count(r => r.First.Contains(r.Second) || r.Second.Contains(r.First));
+
+        public override object SolvePart2() => GetAssignmentsPairs()
+            .Count(r => r.First.Overlaps(r.Second));
+
+        private IEnumerable<AssignmentsPair> GetAssignmentsPairs() =>
+            from line in InputData.GetInputLines()
+            let match = Regex.Match(line, @"(\d+)-(\d+),(\d+)-(\d+)")
+            select new AssignmentsPair(
+                new Range(match.GetInt32Group(1), match.GetInt32Group(2)),
+                new Range(match.GetInt32Group(3), match.GetInt32Group(4)));
     }
 }
