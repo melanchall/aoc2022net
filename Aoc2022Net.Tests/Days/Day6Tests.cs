869583073e4cb36f76d28e3fa55491e63fe4e483
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-12-06 11:20:09 +0300

    Day 6 solved

--- Aoc2022Net.Tests/Days/Day6Tests.cs
+++ Aoc2022Net.Tests/Days/Day6Tests.cs
@@ -3,8 +3,18 @@ using NUnit.Framework;
 
 namespace Aoc2022Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [DayDataPart1("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [DayDataPart1("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [DayDataPart1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [DayDataPart1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    [DayDataPart1(1833)]
+    [DayDataPart2("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    [DayDataPart2("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    [DayDataPart2("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    [DayDataPart2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    [DayDataPart2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    [DayDataPart2(3425)]
     [TestFixture]
     public sealed class Day6Tests : DayTests<Day6Tests>
     {
--- Aoc2022Net/Days/Day6.cs
+++ Aoc2022Net/Days/Day6.cs
@@ -2,8 +2,17 @@
 {
     internal sealed class Day6 : Day
     {
-        public override object SolvePart1() => null;
+        public override object SolvePart1() => GetMarkerPosition(4);
 
-        public override object SolvePart2() => null;
+        public override object SolvePart2() => GetMarkerPosition(14);
+
+        private int GetMarkerPosition(int dataPacketSize)
+        {
+            var input = InputData.GetInputText();
+            return Enumerable
+                .Range(0, input.Length)
+                .First(i => input[i..(i + dataPacketSize)].Distinct().Count() == dataPacketSize)
+                + dataPacketSize;
+        }
     }
 }
