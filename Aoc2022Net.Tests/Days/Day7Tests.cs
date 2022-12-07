40612f50c49522a230a8b31eb145fd1ded8794c7
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-12-07 10:55:48 +0300

    Day 7 solved

--- Aoc2022Net.Tests/Days/Day7Tests.cs
+++ Aoc2022Net.Tests/Days/Day7Tests.cs
@@ -3,8 +3,54 @@ using NUnit.Framework;
 
 namespace Aoc2022Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"$ cd /
+                    $ ls
+                    dir a
+                    14848514 b.txt
+                    8504156 c.dat
+                    dir d
+                    $ cd a
+                    $ ls
+                    dir e
+                    29116 f
+                    2557 g
+                    62596 h.lst
+                    $ cd e
+                    $ ls
+                    584 i
+                    $ cd ..
+                    $ cd ..
+                    $ cd d
+                    $ ls
+                    4060174 j
+                    8033020 d.log
+                    5626152 d.ext
+                    7214296 k", 95437)]
+    [DayDataPart1(2104783)]
+    [DayDataPart2(@"$ cd /
+                    $ ls
+                    dir a
+                    14848514 b.txt
+                    8504156 c.dat
+                    dir d
+                    $ cd a
+                    $ ls
+                    dir e
+                    29116 f
+                    2557 g
+                    62596 h.lst
+                    $ cd e
+                    $ ls
+                    584 i
+                    $ cd ..
+                    $ cd ..
+                    $ cd d
+                    $ ls
+                    4060174 j
+                    8033020 d.log
+                    5626152 d.ext
+                    7214296 k", 24933642)]
+    [DayDataPart2(5883165)]
     [TestFixture]
     public sealed class Day7Tests : DayTests<Day7Tests>
     {
--- Aoc2022Net/Days/Day7.cs
+++ Aoc2022Net/Days/Day7.cs
@@ -1,9 +1,90 @@
-﻿namespace Aoc2022Net.Days
+﻿// No Space Left On Device: https://adventofcode.com/2022/day/7
+
+namespace Aoc2022Net.Days
 {
     internal sealed class Day7 : Day
     {
-        public override object SolvePart1() => null;
+        private sealed class Node
+        {
+            public Node(string name, Node parent)
+            {
+                Name = name;
+                Parent = parent;
+            }
 
-        public override object SolvePart2() => null;
+            public string Name { get; }
+
+            public long Size { get; set; }
+
+            public Node Parent { get; }
+
+            public List<Node> Children { get; } = new List<Node>();
+        }
+
+        public override object SolvePart1() => GetDirectories(GetFileSystem())
+            .Where(d => d.Size <= 100000)
+            .Sum(d => d.Size);
+
+        public override object SolvePart2()
+        {
+            var fileSystem = GetFileSystem();
+            var directories = GetDirectories(fileSystem);
+            var requiredSize = 30000000 - (70000000 - fileSystem.Size);
+            return directories
+                .Select(d => d.Size)
+                .OrderBy(s => s)
+                .First(s => s >= requiredSize);
+        }
+
+        private IEnumerable<Node> GetDirectories(Node fileSystem)
+        {
+            var queue = new Queue<Node>(new[] { fileSystem });
+
+            while (queue.Any())
+            {
+                var node = queue.Dequeue();
+                yield return node;
+
+                foreach (var child in node.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+        }
+
+        private Node GetFileSystem()
+        {
+            var root = new Node("/", null);
+            var currentNode = root;
+
+            foreach (var line in InputData.GetInputLines())
+            {
+                var parts = line.Trim('$', ' ').Split(' ');
+                if (parts.Length < 2)
+                    continue;
+
+                switch (parts[0])
+                {
+                    case "cd":
+                        currentNode = parts[1] switch
+                        {
+                            "/" => root,
+                            ".." => currentNode.Parent,
+                            _ => currentNode.Children.OfType<Node>().First(n => n.Name == parts[1])
+                        };
+                        break;
+                    case "dir":
+                        currentNode.Children.Add(new Node(parts[1], currentNode));
+                        break;
+                    default:
+                        var fileSize = long.Parse(parts[0]);
+                        for (var node = currentNode; node != null; node = node.Parent)
+                            node.Size += fileSize;
+                        break;
+                }
+            }
+
+            return root;
+        }
     }
 }
