61690a29b00aa3dee017fb4b705a24c23b60c0c1
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2022-12-08 12:34:44 +0300

    Day 8 solved

--- Aoc2022Net.Tests/Days/Day8Tests.cs
+++ Aoc2022Net.Tests/Days/Day8Tests.cs
@@ -3,8 +3,18 @@ using NUnit.Framework;
 
 namespace Aoc2022Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"30373
+                    25512
+                    65332
+                    33549
+                    35390", 21)]
+    [DayDataPart1(1717)]
+    [DayDataPart2(@"30373
+                    25512
+                    65332
+                    33549
+                    35390", 8)]
+    [DayDataPart2(321975)]
     [TestFixture]
     public sealed class Day8Tests : DayTests<Day8Tests>
     {
--- Aoc2022Net/Days/Day8.cs
+++ Aoc2022Net/Days/Day8.cs
@@ -1,9 +1,85 @@
-﻿namespace Aoc2022Net.Days
+﻿// Treetop Tree House: https://adventofcode.com/2022/day/8
+
+namespace Aoc2022Net.Days
 {
     internal sealed class Day8 : Day
     {
-        public override object SolvePart1() => null;
+        public override object SolvePart1()
+        {
+            var (grid, width, height) = InputData.GetInputInt32Grid(0, 0);
 
-        public override object SolvePart2() => null;
+            var result = width * 2 + (height - 2) * 2;
+
+            for (var x = 1; x < width - 1; x++)
+            {
+                for (var y = 1; y < height - 1; y++)
+                {
+                    var current = grid[x, y];
+
+                    if (GetLeftXs(x).All(xOffset => IsLeftXValid(current, grid, x, y, xOffset)) ||
+                        GetRightXs(x, width).All(newX => IsRightXValid(current, grid, newX, y)) ||
+                        GetTopYs(y).All(yOffset => IsTopYValid(current, grid, x, y, yOffset)) ||
+                        GetBottomYs(y, height).All(newY => IsBottomYValid(current, grid, x, newY)))
+                        result++;
+                }
+            }
+
+            return result;
+        }
+
+        public override object SolvePart2()
+        {
+            var (grid, width, height) = InputData.GetInputInt32Grid(0, 0);
+
+            var result = 0;
+
+            int Count(IEnumerable<int> values, Func<int, bool> predicate)
+            {
+                var count = 0;
+
+                foreach (var c in values)
+                {
+                    count++;
+                    if (!predicate(c))
+                        break;
+                }
+
+                return count;
+            }
+
+            for (var x = 1; x < width - 1; x++)
+            {
+                for (var y = 1; y < height - 1; y++)
+                {
+                    var current = grid[x, y];
+                    var score =
+                        Count(GetLeftXs(x), xOffset => IsLeftXValid(current, grid, x, y, xOffset)) *
+                        Count(GetRightXs(x, width), newX => IsRightXValid(current, grid, newX, y)) *
+                        Count(GetTopYs(y), yOffset => IsTopYValid(current, grid, x, y, yOffset)) *
+                        Count(GetBottomYs(y, height), newY => IsBottomYValid(current, grid, x, newY));
+                    result = Math.Max(result, score);
+                }
+            }
+
+            return result;
+        }
+
+        private IEnumerable<int> GetLeftXs(int x) =>
+            Enumerable.Range(0, x);
+        private IEnumerable<int> GetRightXs(int x, int width) =>
+            Enumerable.Range(x + 1, width - x - 1);
+        private IEnumerable<int> GetTopYs(int y) =>
+            Enumerable.Range(0, y);
+        private IEnumerable<int> GetBottomYs(int y, int height) =>
+            Enumerable.Range(y + 1, height - y - 1);
+
+        private bool IsLeftXValid(int current, int[,] grid, int x, int y, int offset) =>
+            grid[x - offset - 1, y] < current;
+        private bool IsRightXValid(int current, int[,] grid, int x, int y) =>
+            grid[x, y] < current;
+        private bool IsTopYValid(int current, int[,] grid, int x, int y, int offset) =>
+            grid[x, y - offset - 1] < current;
+        private bool IsBottomYValid(int current, int[,] grid, int x, int y) =>
+            grid[x, y] < current;
     }
 }
